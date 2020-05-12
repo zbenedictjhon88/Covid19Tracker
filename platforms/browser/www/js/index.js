46f5c4ffc1f98d19af8c5f5357aa661292470813
John Benedict Santos <40460523+zbenedictjhon88@users.noreply.github.com>
2020-05-12 10:50:39 +0800

    try to debug data in global

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -17,44 +17,90 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-     
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // Application Constructor
+  initialize: function () {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function () {
+    document.addEventListener("deviceready", this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function () {
+    app.receivedEvent("deviceready");
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function (id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector(".listening");
+    var receivedElement = parentElement.querySelector(".received");
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    listeningElement.setAttribute("style", "display:none;");
+    receivedElement.setAttribute("style", "display:block;");
 
-        global();
+    $.ajax({
+        type: "GET",
+        url: "https://api.thevirustracker.com/free-api?global=stats",
+        dataType: "JSON",
+        success: function (response) {
+          var field = response.results[0];
+          console.log(field);
+          $("#total_cases").html(number_format(field.total_cases, "", "", ","));
+          $("#total_recovered").html(number_format(field.total_recovered, "", "", ","));
+          $("#total_unresolved").html(number_format(field.total_unresolved, "", "", ","));
+          $("#total_deaths").html(number_format(field.total_deaths, "", "", ","));
+          $("#total_new_cases_today").html(number_format(field.total_new_cases_today, "", "", ","));
+          $("#total_new_deaths_today").html(number_format(field.total_new_deaths_today, "", "", ","));
+          $("#total_active_cases").html(number_format(field.total_active_cases, "", "", ","));
+          $("#total_serious_cases").html(number_format(field.total_serious_cases, "", "", ","));
+          $("#total_affected_countries").html(number_format(field.total_affected_countries, "", "", ","));
+          $("#source").html(field.source.url);
+        },
+        error: function (response) {
+          console.log(response);
+        },
+      });
 
-        console.log('Received Event: ' + id);
-    }
+    global();
+
+    console.log("Received Event: " + id);
+  },
 };
 
+function number_format(number, decimals, dec_point, thousands_sep) {
+  // Strip all characters but numerical ones.
+  number = (number + "").replace(/[^0-9+\-Ee.]/g, "");
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = typeof thousands_sep === "undefined" ? "," : thousands_sep,
+    dec = typeof dec_point === "undefined" ? "." : dec_point,
+    s = "",
+    toFixedFix = function (n, prec) {
+      var k = Math.pow(10, prec);
+      return "" + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split(".");
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || "").length < prec) {
+    s[1] = s[1] || "";
+    s[1] += new Array(prec - s[1].length + 1).join("0");
+  }
+  return s.join(dec);
+}
+
 function global() {
-	$.get('components/global/global.html', function(data){ 
-        $("#global").addClass("w3-theme");
-        $('main').html(data);
-        $.getScript("components/global/global.js");
-	});
+  $.get("components/global/global.html", function (data) {
+    $("#global").addClass("w3-theme");
+    $("main").html(data);
+    $.getScript("components/global/global.js");
+  });
 }

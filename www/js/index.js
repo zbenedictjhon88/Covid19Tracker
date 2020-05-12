89a5f0d2642e84fc7939052e64bc35ca5f43b14b
John Benedict Santos <40460523+zbenedictjhon88@users.noreply.github.com>
2020-05-12 23:54:25 +0800

    update for testing app

--- www/js/index.js
+++ www/js/index.js
@@ -51,6 +51,28 @@ var app = {
 };
 
 function global() {
+  $.ajax({
+    type: "GET",
+    url: "https://api.thevirustracker.com/free-api?global=stats",
+    dataType: "JSON",
+    success: function (response) {
+      var field = response.results[0];
+      console.log(field);
+      $("#total_cases").html(number_format(field.total_cases, "", "", ","));
+      $("#total_recovered").html(number_format(field.total_recovered, "", "", ","));
+      $("#total_unresolved").html(number_format(field.total_unresolved, "", "", ","));
+      $("#total_deaths").html(number_format(field.total_deaths, "", "", ","));
+      $("#total_new_cases_today").html(number_format(field.total_new_cases_today, "", "", ","));
+      $("#total_new_deaths_today").html(number_format(field.total_new_deaths_today, "", "", ","));
+      $("#total_active_cases").html(number_format(field.total_active_cases, "", "", ","));
+      $("#total_serious_cases").html(number_format(field.total_serious_cases, "", "", ","));
+      $("#total_affected_countries").html(number_format(field.total_affected_countries, "", "", ","));
+      $("#source").html(field.source.url);
+    },
+    error: function (response) {
+      console.log(response);
+    },
+  });
   $.get("components/global/global.html", function (data) {
     $("#global").addClass("w3-theme");
     $("main").html(data);
